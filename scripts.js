8b3ad1632d5c98dc37e7f3d8af8e401b93d4e02f
gustavoSutil <4541082348@estudante.sed.sc.gov.br>
2022-07-01 03:27:55 -0300

    alteracoes no iframe id:#movie

---
+++ scripts.js
@@ -0,0 +1,3 @@
+timeLasted = document.getElementsByClassName("ytp-time-current");
+timePercored = document.getElementsByClassName("ytp-time-duration");
+console.alert(timeLasted[0],"----",timePercored[0]);
