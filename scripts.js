443b9b924f9cf6109b01b3884dbe154d17e29cc4
gustavoSutil <4541082348@estudante.sed.sc.gov.br>
2022-07-06 13:26:58 -0300

    building texts

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
+//alert text
 function alertCopiado(mensage){
     navigator.clipboard.writeText(mensage);
     window.alert("O texto foi copiado:\n" + mensage);
+}
+
+
+//abre e e fecha menu no celular
+
+var aberto = false;
+function setContato(){
+    var lista = document.getElementById('list');
+    if (aberto==false){
+        aberto=true;
+        lista.style.display = "block";
+    }else{
+        aberto=false;
+        lista.style.display = "none";
+    }
 }
